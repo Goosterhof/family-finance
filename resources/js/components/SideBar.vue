3de5e254b947e3d38376cdf82747833fb8e896b2
Gerard Oosterhof <gerard@script.nl>
2022-01-10 12:15:29 +0100

    Sidebar is mobile friendly

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -1,5 +1,17 @@
 <template>
-    <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <!-- TODO :: make mobile friendly -->
+    <button
+        class="navbar-toggler d-md-none collapsed mb-3 navbar-light"
+        type="button"
+        @click="mobileDropdown = !mobileDropdown"
+    >
+        <span class="navbar-toggler-icon" />
+    </button>
+    <nav
+        id="sidebar"
+        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+        :class="{show: mobileDropdown && isMobile}"
+    >
         <div class="position-sticky">
             <ul class="nav flex-column">
                 <li class="nav-item">
@@ -140,10 +152,12 @@
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                         >
+                            <!-- eslint-disable max-len -->
                             <path
                                 fill-rule="evenodd"
                                 d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
                             />
+                            <!-- eslint-enable max-len -->
                         </svg>
                         <span class="ms-3">Read tutorial</span>
                     </a>
@@ -164,6 +178,12 @@
     </nav>
 </template>
 
+<script setup lang="ts">
+import {isMobile} from 'helpers/mobile';
+import {ref} from 'vue';
+const mobileDropdown = ref(false);
+</script>
+
 <style scoped>
 .sidebar {
     position: fixed;
@@ -179,9 +199,14 @@
 
 @media (max-width: 767.98px) {
     .sidebar {
-        top: 11.5rem;
+        top: 10.5rem;
         padding: 0;
     }
+}
+
+.sidebar.show {
+    max-width: 100%;
+    width: 100%;
 }
 
 .sidebar .nav-link {
--- resources/js/components/TopBar.vue
+++ resources/js/components/TopBar.vue
@@ -2,17 +2,6 @@
     <nav class="navbar navbar-light bg-light p-3">
         <div class="d-flex col-12 col-md-3 col-lg-2 mb-2 mb-lg-0 flex-wrap flex-md-nowrap justify-content-between">
             <a class="navbar-brand" href="#">Family Finance</a>
-            <button
-                class="navbar-toggler d-md-none collapsed mb-3"
-                type="button"
-                data-toggle="collapse"
-                data-target="#sidebar"
-                aria-controls="sidebar"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon" />
-            </button>
         </div>
         <div class="col-12 col-md-4 col-lg-2">
             <input class="form-control form-control-dark" type="text" placeholder="Zoeken" aria-label="Search" />
@@ -25,7 +14,7 @@
                     :class="{show: profileDropdown}"
                     @click="profileDropdown = !profileDropdown"
                 >
-                    Hello, {{ loggedInUser.first_name }}
+                    Hallo, {{ loggedInUser.first_name }}
                 </button>
                 <ul class="dropdown-menu" :class="{show: profileDropdown}">
                     <!-- <li><a class="dropdown-item" href="#">Settings</a></li>

8f10eeda391ad73bfd86d44e0a8aed65ac2972cf
Goosterhof <gerard.oosterhof@gmail.com>
2022-09-18 23:48:32 +0200

    Overview shows table of statements

--- resources/js/domains/statements/pages/Overview.vue
+++ resources/js/domains/statements/pages/Overview.vue
@@ -1,26 +1,56 @@
 <template>
     <p>Afschriften</p>
-    <form @submit.prevent="test">
+    <!-- TODO :: make pretty -->
+    <form @submit.prevent="getCSVAndParseIt">
         <input ref="csvInput" type="file" accept=".csv" />
-        <br />
         <input type="submit" value="Submit" />
     </form>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Rekening</th>
+                <th>Datum</th>
+                <th>Bedrag</th>
+                <th>Naar rekening</th>
+                <th>Naar rekening van</th>
+                <th>Beschrijving</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="statement in statements" :key="statement.bankId">
+                <td>{{ statement.account }}</td>
+                <td>{{ statement.transactionDate }}</td>
+                <td>{{ statement.amount }}</td>
+                <td>{{ statement.toAccount }}</td>
+                <td>{{ statement.toAccountName }}</td>
+                <td>{{ statement.description }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script setup lang="ts">
+import {New} from 'types/generics';
+import {Statement} from '../types';
 import {parseCSV} from 'helpers/csv';
 import {ref} from 'vue';
 
-const csvInput = ref<HTMLInputElement>();
+const FIRST_FILE_INDEX = 0;
 
-const test = () => {
+const csvInput = ref<HTMLInputElement>();
+const statements = ref<New<Statement>[]>([]);
+
+const getCSVAndParseIt = () => {
     if (!csvInput.value || !csvInput.value.files) return;
-    const input = csvInput.value.files[0];
+    const input = csvInput.value.files[FIRST_FILE_INDEX];
+    if (!input) return;
     const reader = new FileReader();
 
-    reader.onload = function (e) {
-        const text = e.target.result;
-        console.log(parseCSV(text));
+    reader.onload = ({target}: ProgressEvent<FileReader>) => {
+        if (!target || !target.result) return;
+        const text = target.result;
+        statements.value = parseCSV(text.toString());
     };
 
     reader.readAsText(input);

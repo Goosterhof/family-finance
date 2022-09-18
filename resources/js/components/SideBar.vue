7efc757c1a8da785aa3a5d13eb59349634b3e6fa
Goosterhof <gerard.oosterhof@gmail.com>
2022-09-18 15:03:22 +0200

    Added a first reference in the sidebar

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -50,7 +50,7 @@
                             <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z" />
                             <polyline points="13 2 13 9 20 9" />
                         </svg>
-                        <span class="ms-3">Orders</span>
+                        <span class="ms-3">Afschriften</span>
                     </a>
                 </li>
                 <li class="nav-item">

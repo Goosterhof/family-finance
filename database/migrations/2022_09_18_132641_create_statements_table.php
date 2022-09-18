09bc130b97ab83bf022388492e58bc428fee41ee
Goosterhof <gerard.oosterhof@gmail.com>
2022-09-18 15:45:26 +0200

    Updated migration

--- database/migrations/2022_09_18_132641_create_statements_table.php
+++ database/migrations/2022_09_18_132641_create_statements_table.php
@@ -4,6 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+// "IBAN/BBAN",
+// "Volgnr",
+// "Datum",
+// "Bedrag",
+// "Saldo na trn",
+// "Tegenrekening IBAN/BBAN",
+// "Naam tegenpartij",
+// "Omschrijving-1",
+
 return new class extends Migration
 {
     /**
@@ -15,6 +24,17 @@ return new class extends Migration
     {
         Schema::create('statements', function (Blueprint $table) {
             $table->id();
+            // TODO :: should become foreign
+            $table->string('account');
+            $table->string('bank_id');
+            $table->date('transaction_date');
+            $table->float('amount', 2);
+            $table->float('balance_after', 2);
+            // TODO :: should become foreign
+            $table->string('to_account');
+            $table->string('to_account_name');
+            $table->string('description');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }

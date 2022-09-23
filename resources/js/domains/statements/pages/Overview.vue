6c299f476aab2702325f9b0d4f3c5f100dc0d4c6
Goosterhof <gerard.oosterhof@gmail.com>
2022-09-23 13:58:19 +0200

    Statement overview page refactor

--- resources/js/domains/statements/pages/Overview.vue
+++ resources/js/domains/statements/pages/Overview.vue
@@ -1,38 +1,39 @@
 <template>
-    <p>Afschriften</p>
+    <h1 class="h1">Afschriften</h1>
 
-    <!-- TODO :: make pretty -->
-    <form v-if="!Object.values(statementsPerAccountName).length" @submit.prevent="getCSVAndParseIt">
-        <input ref="csvInput" type="file" accept=".csv" />
-        <input type="submit" value="Submit" />
+    <form v-if="!statements.length">
+        <input ref="csvInput" type="file" accept=".csv" @input="getCSVAndParseIt" />
     </form>
 
     <template v-else>
-        <h1 class="h2">
-            {{ Object.values(statementsPerAccountName)[0][0].account }}
-        </h1>
+        <div class="d-flex justify-content-between align-items-center">
+            <h1 class="h2">
+                {{ statements[0].account }}
+            </h1>
+            <span :style="getStyleForPrice(totalOveral)">€{{ totalOveral }}</span>
+        </div>
 
         <div
-            v-for="(statements, toAccountName, index) in statementsPerAccountName"
+            v-for="(statementsForAccountName, toAccountName, index) in statementsPerAccountName"
             :key="toAccountName"
             class="card mb-2 pb-2"
             @click="toggleCollapsable(index)"
         >
             <div class="card-header d-flex justify-content-between align-items-center">
                 <h6 class="card-subtitle">{{ toAccountName }}</h6>
-                <span>€{{ statements.reduce((acc, {amount}) => (acc += amount), 0) }}</span>
+                <span :style="getStyleForPrice(totalPerAccountName[toAccountName])">
+                    €{{ totalPerAccountName[toAccountName] }}
+                </span>
             </div>
             <div ref="collapsableElements">
-                <div v-for="statement in statements" :key="statement.description" class="card ms-2 me-2 mt-2">
+                <div
+                    v-for="statement in statementsForAccountName"
+                    :key="statement.description"
+                    class="card ms-2 me-2 mt-2"
+                >
                     <div class="card-body">
                         <div>
-                            <span
-                                :style="{
-                                    color: statement.amount < 0 ? 'red' : 'green',
-                                }"
-                            >
-                                €{{ statement.amount }}
-                            </span>
+                            <span :style="getStyleForPrice(statement.amount)">€{{ statement.amount }}</span>
                             <span class="ms-2">
                                 <select v-model="statement.categoryId">
                                     <option disabled :value="0">Nog geen optie toegevoegd</option>
@@ -54,11 +55,12 @@
 import {Collapse} from 'bootstrap';
 import {New} from 'types/generics';
 import {Statement} from '../types';
+import {StyleValue, computed, reactive, ref, watchEffect} from 'vue';
 import {categoryRepository, categoryStoreModule} from 'domains/categories';
 import {parseCSV} from 'helpers/csv';
-import {ref, watchEffect} from 'vue';
 
 const FIRST_FILE_INDEX = 0;
+const TOTAL_START_COUNT = 0;
 
 const collapsableElements = ref<HTMLDivElement[]>([]);
 const collapsables = ref<Collapse[]>([]);
@@ -68,12 +70,16 @@ watchEffect(() => (collapsables.value = collapsableElements.value.map(el => new 
 const toggleCollapsable = (index: number) => collapsables.value[index].toggle();
 
 const csvInput = ref<HTMLInputElement>();
-const statementsPerAccountName = ref<Record<string, New<Statement>[]>>({});
+const statements = ref<New<Statement>[]>([]);
+const statementsPerAccountName = reactive<Record<string, New<Statement>[]>>({});
+const totalPerAccountName = reactive<Record<string, number>>({});
+
+const totalOveral = computed(() => statements.value.reduce((total, {amount}) => total + amount, TOTAL_START_COUNT));
 
 const accountToCategory: Record<string, number> = {};
 
 watchEffect(() => {
-    const allStatements = Object.values(statementsPerAccountName.value).flat();
+    const allStatements = Object.values(statementsPerAccountName).flat();
     for (const statement of allStatements) {
         if (statement.categoryId) {
             accountToCategory[statement.toAccountName] = statement.categoryId;
@@ -103,20 +109,22 @@ const getCSVAndParseIt = () => {
     reader.onload = ({target}: ProgressEvent<FileReader>) => {
         if (!target || !target.result) return;
         const text = target.result;
-        const statements = parseCSV(text.toString());
+        statements.value = parseCSV(text.toString());
 
-        statementsPerAccountName.value = statements.reduce<Record<string, New<Statement>[]>>((acc, statement) => {
-            if (!acc[statement.toAccountName]) acc[statement.toAccountName] = [];
-            acc[statement.toAccountName].push(statement);
-            return acc;
-        }, {});
+        for (const statement of statements.value) {
+            if (!statementsPerAccountName[statement.toAccountName]) {
+                statementsPerAccountName[statement.toAccountName] = [];
+                totalPerAccountName[statement.toAccountName] = 0;
+            }
+            statementsPerAccountName[statement.toAccountName].push(statement);
+            totalPerAccountName[statement.toAccountName] += statement.amount;
+        }
     };
 
     reader.readAsText(input);
 };
 
-// onMounted(() => {
-//     if (!collapsable.value) return;
-//     collapsable.value.forEach(el => new Collapse(el));
-// });
+const getStyleForPrice = (price: number): StyleValue => ({
+    color: price < TOTAL_START_COUNT ? 'red' : 'green',
+});
 </script>
